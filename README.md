e3cc8c370f31b8c7467af19d0accc8e3491641de
Tommi Siik <23232855+tsiika@users.noreply.github.com>
2018-03-20 21:39:45 +0200

    Jalmari is alive
    And he is feeling good!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # jalmari-bot
+
+Jalmari Bot is bot designed to Discord VoIP software. I'm sure there is lot of these bot's but hey, why not build own?
+
+Bot is build with Javascript and runs on NodeJS.
---
+++ jalmari.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.on("ready", () => {
+    console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+    if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    if (message.content.startsWith(config.prefix +"ping")) {
+        message.channel.send("Pong!");
+    }
+});
+
+client.login(config.token);
